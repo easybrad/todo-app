76eddc56231c05fa5e7ff07c588e8c619d09fdf8
brad-mac <brad.ma.mail@gmail.com>
2023-11-07 23:27:42 +0800

    add index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // 假设App.tsx在同一目录下
-
+import './index.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
